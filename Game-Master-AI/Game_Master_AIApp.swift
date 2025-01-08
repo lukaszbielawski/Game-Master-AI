e2adcd6e0781af8c49868ca416334634bb986d8c
lukaszbielawski <lukasz.bi@onet.pl>
2025-01-08 14:03:27 +0100

    Initial Commit

---
+++ Game-Master-AI/Game_Master_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Master_AIApp.swift
+//  Game-Master-AI
+//
+//  Created by Łukasz Bielawski on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Game_Master_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
